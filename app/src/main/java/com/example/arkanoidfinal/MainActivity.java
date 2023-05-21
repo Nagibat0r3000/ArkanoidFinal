6dc6e4b55f893fffc2727f393e84164f8456f4ec
Nagibat0r3000 <kelmington@bk.ru>
2023-05-21 19:15:34 +0700

    Initial commit

--- app/src/main/java/com/example/arkanoidfinal/MainActivity.java
+++ app/src/main/java/com/example/arkanoidfinal/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -22,12 +23,13 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
     int startchet=4;
     int ballx=15;int bally=-15;
+    int score=0; int level=1;
+    int bricks=81;
     int respawn=0;
     ImageView plat;
-    int ballstart;int platstart;
+    int ballstart;int platstart; int ballstartx;
     ImageView ball;Handler handler = new Handler();
-    //ImageView[][] bricks = new ImageView[9][9];
-         ImageView[] bricks1 = new ImageView[3];
+    ImageView[] bricks1 = new ImageView[81];
 
     Runnable runnable = new Runnable() {
         @Override
@@ -38,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                     plat.setX(x - 15);
                 }
             }
-            handler.postDelayed(this, 20);
+            handler.postDelayed(this, 10);
         }
     };
     Handler handler2 = new Handler();
@@ -51,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                     plat.setX(x + 15);
                 }
             }
-            handler2.postDelayed(this, 20);
+            handler2.postDelayed(this, 10);
         }
     };
 
@@ -59,21 +61,19 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //for (int i = 0; i < 81; i++) {
-        for(int i =0; i< 3;i++){
+        for(int i =0; i<81;i++){
             int id = getResources().getIdentifier("a" + i, "id", getPackageName());
             bricks1[i] = findViewById(id);
         }
         plat = findViewById(R.id.Platforma);
-        plat.setX(plat.getX()+700);
-        platstart=(int)plat.getX();
+        ImageView wal = findViewById(R.id.leftwall);
+        ImageView walr = findViewById(R.id.rightwall);
          ball = findViewById(R.id.ballid);
-        ball.setX(1180);
-        ballstart=(int)ball.getY();
+
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
@@ -88,6 +88,18 @@ public class MainActivity extends AppCompatActivity {
                             timer.cancel();
                             ViewGroup parent = (ViewGroup) textView.getParent();
                             parent.removeView(textView);
+                            ImageView wal = findViewById(R.id.leftwall);
+                            ImageView walr = findViewById(R.id.rightwall);
+                            ball.setX(plat.getRight()-150);
+                            ballstart=(int)ball.getY();
+                            ballstartx = (int)ball.getX();
+                            platstart=(int)plat.getX();
+                            //for (int i = 0; i < 81; i++) {
+                             //   if(i!=8) {
+                             //       bricks1[i].setVisibility(View.INVISIBLE);
+                            //        bricks--;
+                            //    }
+                            //}
                         }
                         startchet--;
                     }
@@ -127,10 +139,35 @@ public class MainActivity extends AppCompatActivity {
                 if(startchet==-1 && respawn==0){
                     ball.setX(ball.getX() + ballx);
                     ball.setY(ball.getY() + bally);
-                    if(ball.getX() == 190) {
+                    for (int i = 0; i < 81; i++) {
+                        ImageView img = bricks1[i];
+                        Rect rect1 = new Rect();
+                        ball.getGlobalVisibleRect(rect1);
+
+                        Rect rect2 = new Rect();
+                        bricks1[i].getGlobalVisibleRect(rect2);
+
+                        if (Rect.intersects(rect1, rect2)) {
+                            if(img.getVisibility()==View.VISIBLE)
+                            {
+                                bally=-bally;
+                                score+=100;
+                                bricks-=1;
+                                if(bricks==0)
+                                {
+                                    nextlevel();
+                                }
+                                TextView txt = findViewById(R.id.scoreView);
+                                txt.setText("Score:\n" + Integer.toString(score));
+                            }
+                            img.setVisibility(View.GONE);
+                        }
+                    }
+                    if(ball.getX() <= wal.getRight()) {
                         ballx = -ballx;
                     }
-                    if(ball.getX() == 2230){
+
+                    if(ball.getX() >= walr.getX()-60){
                         ballx = -ballx;
                     }
                     if(ball.getY() <= 0) {
@@ -138,22 +175,28 @@ public class MainActivity extends AppCompatActivity {
                     }
                     if(ball.getY() + ball.getHeight() >= plat.getY() && ball.getY() <= plat.getY() + plat.getHeight() && ball.getX() + ball.getWidth() >= plat.getX() && ball.getX() <= plat.getX() + plat.getWidth()) {
                         bally = -bally;
+                        if (ball.getX() + 80 == plat.getX() || ball.getLeft() == plat.getX() +240) {
+                            ballx=-ballx;
+                        }
                     }
                     if(ball.getY()>=plat.getY())
                     {
                         upal();
                     }
                 }
-                handler12.postDelayed(this, 20);
+                handler12.postDelayed(this, 30);
             }
         };
-        handler12.postDelayed(runnable12, 20);
+        handler12.postDelayed(runnable12, 30);
 
     }
     public void upal() {
+        score-=500;
+        TextView txt = findViewById(R.id.scoreView);
+        txt.setText("Score:\n" + Integer.toString(score));
         respawn=1;
         ConstraintLayout cl = findViewById(R.id.rella);
-        ball.setX(1210);ball.setY(1200);plat.setX(platstart+300);
+        ball.setX(ballstartx);ball.setY(ballstart);plat.setX(ball.getLeft()-100);
         ballx=15;bally=-15;
         //  startchet=50;
         final TextView countdownTextView = new TextView(this);
@@ -184,5 +227,33 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
     }
-
+    public void nextlevel(){
+            upal();
+            score+=500;
+            TextView txt = findViewById(R.id.scoreView);
+            txt.setText("Score:\n" + Integer.toString(score));
+            level++;
+            switch (level){
+                case 2:
+                    for (int i = 0; i < 81; i++) {
+                        if(i%9==0 || i==0)
+                        {
+                            ImageView img = bricks1[i];
+                            img.setVisibility(View.VISIBLE);
+                        }
+                        bricks=9;
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < 81; i++) {
+                        if(i%9==1 || i==1)
+                        {
+                            ImageView img = bricks1[i];
+                            img.setVisibility(View.VISIBLE);
+                        }
+                        bricks=9;
+                    }
+                    break;
+            }
+    }
 }
